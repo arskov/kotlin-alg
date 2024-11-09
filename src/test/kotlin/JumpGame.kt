acce4710c91703e24cbc233ef74b5f429ed24c7f
Arseni Kavalchuk <ars.kov@gmail.com>
2024-11-09 18:27:51 +0000

    Update to Game Jump

--- src/test/kotlin/JumpGame.kt
+++ src/test/kotlin/JumpGame.kt
@@ -18,6 +18,29 @@ object JumpGame {
             }
             return canJumpInternal(nums, 0)
         }
+
+        fun canJumpBacktrackingMemo(nums: IntArray): Boolean {
+            val memo = mutableMapOf<Int, Boolean?>()
+            fun canJumpInternal(nums: IntArray, i: Int): Boolean {
+                memo[i]?.let {
+                    return it
+                }
+                if (i == nums.size - 1) {
+                    memo[i] = true
+                    return true
+                }
+                val maxJump = minOf(nums[i] + i, nums.size - 1)
+                for (j in ((i + 1)..maxJump)) {
+                    if (canJumpInternal(nums, j)) {
+                        memo[i] = true
+                        return true
+                    }
+                }
+                memo[i] = false
+                return false
+            }
+            return canJumpInternal(nums, 0)
+        }
     }
 
     @Test
@@ -25,5 +48,7 @@ object JumpGame {
         val solution = Solution()
         assertEquals(true, solution.canJumpBacktracking(intArrayOf(2, 3, 1, 1, 4)))
         assertEquals(false, solution.canJumpBacktracking(intArrayOf(3, 2, 1, 0, 4)))
+        assertEquals(true, solution.canJumpBacktrackingMemo(intArrayOf(2, 3, 1, 1, 4)))
+        assertEquals(false, solution.canJumpBacktrackingMemo(intArrayOf(3, 2, 1, 0, 4)))
     }
 }
